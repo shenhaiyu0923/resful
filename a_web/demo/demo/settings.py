5481eab2cb44d794497c100108c6333e8d1d0d52
jwang9 <jwang9@vova.com.hk>
2020-03-04 10:10:49 +0800

    补充drf框架sql

--- a_web/demo/demo/settings.py
+++ a_web/demo/demo/settings.py
@@ -121,20 +121,20 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # 配置输出sql
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console':{
-            'level':'DEBUG',
-            'class':'logging.StreamHandler',
-        },
-    },
-    'loggers': {
-        'django.db.backends': {
-            'handlers': ['console'],
-            'propagate': True,
-            'level':'DEBUG',
-        },
-    }
-}
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'handlers': {
+#         'console':{
+#             'level':'DEBUG',
+#             'class':'logging.StreamHandler',
+#         },
+#     },
+#     'loggers': {
+#         'django.db.backends': {
+#             'handlers': ['console'],
+#             'propagate': True,
+#             'level':'DEBUG',
+#         },
+#     }
+# }
