e0d44c5b0edd5dae1c0d1af432f550ebb2181736
jwang9 <jwang9@vova.com.hk>
2020-03-11 11:12:51 +0800

    genericapiview_view

--- a_web/demo/book_drf/genericapiview_view.py
+++ a_web/demo/book_drf/genericapiview_view.py
@@ -2,16 +2,18 @@ from django.http import JsonResponse
 from book_drf.serializer import BookSerializer
 from books.models import BookInfo
 from rest_framework.views import APIView
+from rest_framework.request import Request
 from rest_framework.response import Response
+from rest_framework.generics import GenericAPIView
+# Create your views here.
 
-
-class Books(APIView):
-
+class Books(GenericAPIView):
+    queryset = BookInfo.objects.all() #指定当前类视图使用的查询集数据
+    serializer_class = BookSerializer  #指定当前类视图使用的序列化器
     def get(self,request):
         # 查询所有图书对象
-        print(request.query_params)
-        books = BookInfo.objects.all()
-        ser = BookSerializer(books,many=True)# 查询多个
+        books = self.get_queryset() # 获取查询集中的所有的数据
+        ser = self.get_serializer(books,many=True)# 使用指定序列化器，获取序列化器对象
         return Response(ser.data)
     def post(self, request):
         # 1、获取前端数据
@@ -19,7 +21,7 @@ class Books(APIView):
         # data_dict = json.loads(data)
         data = request.data
         # 2、验证数据
-        ser= BookSerializer(data=data)
+        ser= self.get_serializer(data=data)
         # ser.is_valid(raise_exception=True) # 验证方法
         ser.is_valid() # 验证方法
         print(ser.validated_data)#验证正确后的数据
@@ -30,7 +32,7 @@ class Books(APIView):
         # 4、返回结果
         return Response(ser.data)
 
-class Book(APIView):
+class Book(GenericAPIView):
 
     def get(self,request,pk):
         #  查询所有图书对象
@@ -39,10 +41,14 @@ class Book(APIView):
         return Response(ser.data)
 
 
-class BookDRFView(APIView):
+class BookDRFView(GenericAPIView):
     """
         获取单一和更新和删除
     """
+    queryset = BookInfo.objects.all()  #指定当前类视图使用的查询集数据
+    serializer_class = BookSerializer  #指定当前类视图使用的序列化器
+
+
     def put(self, request, pk):
         # 1、获取前端数据
         # data = request.body.decode()
@@ -50,7 +56,7 @@ class BookDRFView(APIView):
         data = request.data
         # 2、验证数据
         try:
-            book = BookInfo.objects.get(id=pk)
+            book = self.get_object() # 从查询集中获取指定的单一数据对象
         except:
             return JsonResponse({'error': '错误信息'}, status=400)
         ser=BookSerializer(book,data=data)
--- a_web/demo/book_drf/urls.py
+++ a_web/demo/book_drf/urls.py
@@ -1,13 +1,17 @@
 from django.conf.urls import url
 
-from book_drf import apiview_view
+from book_drf import apiview_view, genericapiview_view
 from  . import  views
 urlpatterns = [
     # url(r'^books_drf$', views.Books.as_view()),
     # url(r'^book_drf/(?P<pk>\d+)$', views.Book.as_view()),
     # url(r'^books_drf/(?P<pk>\d+)$', views.BookDRFView.as_view()),
 
-    url(r'^books_drf$', apiview_view.Books.as_view()),
-    url(r'^book_drf/(?P<pk>\d+)$', apiview_view.Book.as_view()),
-    url(r'^books_drf/(?P<pk>\d+)$', apiview_view.BookDRFView.as_view()),
+    # url(r'^books_drf$', apiview_view.Books.as_view()),
+    # url(r'^book_drf/(?P<pk>\d+)$', apiview_view.Book.as_view()),
+    # url(r'^books_drf/(?P<pk>\d+)$', apiview_view.BookDRFView.as_view()),
+
+    url(r'^books_drf$', genericapiview_view.Books.as_view()),
+    url(r'^book_drf/(?P<pk>\d+)$', genericapiview_view.Book.as_view()),
+    url(r'^books_drf/(?P<pk>\d+)$', genericapiview_view.BookDRFView.as_view()),
 ]
