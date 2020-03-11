4ef3bc8a3bca054f69c0e144d1032340433057d5
jwang9 <jwang9@vova.com.hk>
2020-03-11 10:25:53 +0800

    APIview_view

--- a_web/demo/book_drf/apiview_view.py
+++ a_web/demo/book_drf/apiview_view.py
@@ -6,16 +6,26 @@ from django.views import View
 from book_drf.serializer import BookSerializer
 from book_drf.serializer import BookModelSerialzier
 from books.models import BookInfo
+from rest_framework.views import APIView
+from rest_framework.request import Request
+from rest_framework.response import Response
 # Create your views here.
 
-class Books(View):
+class Books(APIView):
 
+    def get(self,request):
+        # 查询所有图书对象
+        print(request.query_params)
+        books = BookInfo.objects.all()
+        ser = BookSerializer(books,many=True)# 查询多个
+        return Response(ser.data)
     def post(self, request):
         # 1、获取前端数据
-        data = request.body.decode()
-        data_dict = json.loads(data)
+        # data = request.body.decode()
+        # data_dict = json.loads(data)
+        data = request.data
         # 2、验证数据
-        ser= BookSerializer(data=data_dict)
+        ser= BookSerializer(data=data)
         # ser.is_valid(raise_exception=True) # 验证方法
         ser.is_valid() # 验证方法
         print(ser.validated_data)#验证正确后的数据
@@ -24,38 +34,37 @@ class Books(View):
         # 3、保存数据
         ser.save()
         # 4、返回结果
-        return JsonResponse(ser.data)
+        return Response(ser.data)
 
-class Book(View):
+class Book(APIView):
 
     def get(self,request,pk):
         #  查询所有图书对象
-
         book = BookInfo.objects.get(id=pk)
         ser = BookSerializer(book)##查询单个
-        print(ser.data)
-        return JsonResponse(ser.data,safe=False,json_dumps_params={'ensure_ascii':False})
+        return Response(ser.data)
 
 
-class BookDRFView(View):
+class BookDRFView(APIView):
     """
         获取单一和更新和删除
     """
     def put(self, request, pk):
         # 1、获取前端数据
-        data = request.body.decode()
-        data_dict = json.loads(data)
+        # data = request.body.decode()
+        # data_dict = json.loads(data)
+        data = request.data
         # 2、验证数据
         try:
             book = BookInfo.objects.get(id=pk)
         except:
             return JsonResponse({'error': '错误信息'}, status=400)
-        ser=BookSerializer(book,data=data_dict)
+        ser=BookSerializer(book,data=data)
         ser.is_valid()
         # 3、更新数据
         ser.save()
         # 4、返回结果
-        return JsonResponse(ser.data)
+        return Response(ser.data)
 
     def delete(self, request, pk):
         # 1、查询数据对象
@@ -69,4 +78,4 @@ class BookDRFView(View):
         #book.save()
         book.delete() #物理删除
 
-        return JsonResponse({})
+        return Response({})
--- a_web/demo/book_drf/urls.py
+++ a_web/demo/book_drf/urls.py
@@ -1,7 +1,13 @@
 from django.conf.urls import url
+
+from book_drf import apiview_view
 from  . import  views
 urlpatterns = [
-    url(r'^books_drf$', views.Books.as_view()),
-    url(r'^book_drf/(?P<pk>\d+)$', views.Book.as_view()),
-    url(r'^books_drf/(?P<pk>\d+)$', views.BookDRFView.as_view()),
+    # url(r'^books_drf$', views.Books.as_view()),
+    # url(r'^book_drf/(?P<pk>\d+)$', views.Book.as_view()),
+    # url(r'^books_drf/(?P<pk>\d+)$', views.BookDRFView.as_view()),
+
+    url(r'^books_drf$', apiview_view.Books.as_view()),
+    url(r'^book_drf/(?P<pk>\d+)$', apiview_view.Book.as_view()),
+    url(r'^books_drf/(?P<pk>\d+)$', apiview_view.BookDRFView.as_view()),
 ]
