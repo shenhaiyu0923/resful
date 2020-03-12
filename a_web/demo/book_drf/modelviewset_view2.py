19d1e64c40cc533c26f20a4a71607b70796cbf89
jwang9 <jwang9@vova.com.hk>
2020-03-12 13:48:04 +0800

    modelview_set

--- a_web/demo/book_drf/modelviewset_view2.py
+++ a_web/demo/book_drf/modelviewset_view2.py
@@ -8,10 +8,20 @@ from books.models import BookInfo
 
 class Books(ModelViewSet):
     queryset = BookInfo.objects.all()  # 指定当前类视图使用的查询集数据
-    serializer_class = BookSerializer  # 指定当前类视图使用的序列化器
+    # serializer_class = BookSerializer  # 指定当前类视图使用的序列化器
+
+    # 这种不常用,一般一个类对应一个序列化器
+    def get_serializer_class(self):
+        if self.action =='lastdata':
+            return BookSerializer
+        elif self.action=='create':
+            return BookSerializer
+        else:
+            return BookSerializer
 
     @action(methods=['get'],detail=True)#detail=True 可以带参数
     def lastdata(self,request,pk):
+        print(self.action)
         book=BookInfo.objects.get(id=pk)
-        ser=BookSerializer(book)
+        ser=self.get_serializer(book)
         return Response(ser.data)
--- a_web/demo/book_drf/urls.py
+++ a_web/demo/book_drf/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 
-from book_drf import apiview_view, genericapiview_view, mixin_view, childmixin_view, viewset_view, genericviewset_view,modelviewset_view
+from book_drf import apiview_view, genericapiview_view, mixin_view, childmixin_view, viewset_view, genericviewset_view, \
+    modelviewset_view, modelviewset_view2
+from rest_framework.routers import DefaultRouter
 from  . import  views
 urlpatterns = [
     # url(r'^books_drf$', views.Books.as_view()),
@@ -34,7 +36,19 @@ urlpatterns = [
     # url(r'^books_drf/(?P<pk>\d+)/lastdata$', genericviewset_view.BookDRFView.as_view({'get': 'lastdata'})),
 
     # ModelViewSet路由使用
-    url(r'^books_drf$', modelviewset_view.Books.as_view({'get': 'list', 'post': 'create', })),
-    url(r'^books_drf/(?P<pk>\d+)$',modelviewset_view.Books.as_view({'put': 'update', 'delete': 'destroy', 'get': 'retrieve'})),
-    # url(r'^books_drf/(?P<pk>\d+)/lastdata$', modelviewset_view.Books.as_view({'get': 'lastdata'})),
+    # url(r'^books_drf$', modelviewset_view.Books.as_view({'get': 'list', 'post': 'create', })),
+    # url(r'^books_drf/(?P<pk>\d+)$',modelviewset_view.Books.as_view({'put': 'update', 'delete': 'destroy', 'get': 'retrieve'})),
+    # # url(r'^books_drf/(?P<pk>\d+)/lastdata$', modelviewset_view.Books.as_view({'get': 'lastdata'})),
 ]
+
+# # 自动生成路由,要和视图集配合使用
+# router = DefaultRouter()
+# router.register('books_drf', modelviewset_view.Books,basename='books')
+# print(router.urls)# 打印路由
+# urlpatterns += router.urls
+
+# 自定义生成路由,要和视图集配合使用
+router = DefaultRouter()
+router.register('books_drf', modelviewset_view2.Books,basename='books')
+print(router.urls)# 打印路由
+urlpatterns += router.urls
